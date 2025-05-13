cdfa212a829f9755559d10f63b2fe9723c68d69f
annaruntti <anruntti@gmail.com>
2025-05-13 16:24:12 +0300

    style fix for more-stories component

--- app/more-stories.tsx
+++ app/more-stories.tsx
@@ -44,10 +44,10 @@ function PostPreview({
 export default function MoreStories({ morePosts }: { morePosts: any[] }) {
   return (
     <section>
-      <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
+      <h2 className="mb-8 text-4xl md:text-5xl font-semibold tracking-tighter leading-tight">
         Lisää artikkeleita
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-8 lg:gap-x-16 gap-y-20 md:gap-y-32 mb-32">
         {morePosts.map((post) => (
           <div key={post.slug}>
             <div className="mb-5">
